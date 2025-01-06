4156420fcbe2348376ff84dd5b2b10a9b63fad9b
Boy9999rich <boymurodmamarajapov1@gmail.com>
2025-01-06 15:35:40 +0500

    new upload

---
+++ 02__06__2025/02__06__2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02__06__2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
